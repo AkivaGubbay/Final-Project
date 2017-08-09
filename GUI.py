c2da6464531851e8e68ad4e8029b8a99611acddd
AkivaGubbay <gubbayakiva@gmail.com>
2017-08-09 15:59:37 +0300

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -3,19 +3,24 @@ from tkinter import *
 
 import tkinter as tk
 from tkinter import filedialog
+from GoogleSpeechApi import *
+from emph_classifier import *
+from dtwAlgo import *
 
 str1 = '                 '
 str2 = '    '
 frame = None
-temp_Label =None
-s = "c:\\Click_to_change.wav"
+temp_Label = None
+s = '/home/akiva/Desktop/%see%the bombers fly up.wav'
+file_path = '/home/akiva/Desktop/%see%the bombers fly up.wav'
+
 
 def FunForButtonASR(self):
     self.top = Toplevel()
     self.top.title("ASR")
     _Label1 = Label(self.top, text='Click the button below to activate the ASR:')
     _Label1.pack(padx=190, pady=30)
-    _start_asr = Button(self.top, text=str2+'Start ASR'+str2)
+    _start_asr = Button(self.top, text=str2 + 'Start ASR' + str2)
     _start_asr.pack()
     _start_asr.config(font=("Courier", 12))
     Label(self.top, text=' ').pack(padx=190, pady=10)
@@ -28,17 +33,25 @@ def FunForButtonASR(self):
 
     def startGoogleASR(self):
         temp_Label.destroy()
-        Label(frame, text='My name is zvika').pack()
+        #Label(frame, text='Say something!').pack()
+        try:
+            input_speech = run()
+        except:
+            input_speech = 'say that again..please'
+        print(input_speech)
+
+
+
+        Label(frame, text=input_speech).pack()
         Label(frame, text=' ').pack(padx=250, pady=5)
+
     _start_asr.bind('<Button>', startGoogleASR)
 
-    
+
 def FunForButtonClassifier(self):
     self.top = Toplevel()
-    self.top.title("Classifier")
     _Label1 = Label(self.top, text='Write the path of the file and Click the button below to activate the Classifier:')
     _Label1.pack(padx=90, pady=30)
-
 
     def update_btn_text():
         print("There will be a code file uploads")
@@ -46,21 +59,20 @@ def FunForButtonClassifier(self):
             file_path = filedialog.askopenfilename()
             print("file_path " + file_path)
 
-            if (len(file_path) > 4 & file_path[-4:] == ".wav"):
+            if (len(file_path) > 4):
                 s = file_path
                 _change_path.set(str2 + 'Path: ' + s + str2)
         except:
             print('You didn\'t select a file.')
 
-
     _change_path = tk.StringVar()
     btn = tk.Button(self.top, textvariable=_change_path, command=update_btn_text)
-    _change_path.set(str2 + 'Path: '+ s + str2)
+    _change_path.set(str2 + 'Path: ' + s + str2)
     btn.pack()
 
     Label(self.top, text=' ').pack()
 
-    _start_asr = Button(self.top, text=str2+'Start Classifier'+str2)
+    _start_asr = Button(self.top, text=str2 + 'Start Classifier' + str2)
     _start_asr.pack()
     _start_asr.config(font=("Courier", 12))
 
@@ -75,10 +87,14 @@ def FunForButtonClassifier(self):
     def startGoogleASR(self):
 
         temp_Label.destroy()
-        Label(frame, text='File: '+s+' text= My name is zvika.').pack()
+
+        classify(s)
+        Label(frame, text='File: ' + s + ' text= My name is zvika.').pack()
 
         Label(frame, text=' ').pack(padx=250, pady=5)
+
     _start_asr.bind('<Button>', startGoogleASR)
+
 
 def FunForButtonDTW(self):
     self.top = Toplevel()
@@ -86,27 +102,25 @@ def FunForButtonDTW(self):
     _Label1 = Label(self.top, text='Write the path of the file and Click the button below to activate the DTW:')
     _Label1.pack(padx=90, pady=30)
 
-
     def update_btn_text():
         print("There will be a code file uploads")
         try:
             file_path = filedialog.askopenfilename()
             print("file_path " + file_path)
 
-            if (len(file_path) > 4 & file_path[-4:] == ".wav"):
+            if (len(file_path) > 4):
                 s = file_path
                 _change_path.set(str2 + 'Path: ' + s + str2)
         except:
             print('You didn\'t select a file.')
 
-
     _change_path = tk.StringVar()
     btn = tk.Button(self.top, textvariable=_change_path, command=update_btn_text)
-    _change_path.set(str2 + 'Path: '+ s + str2)
+    _change_path.set(str2 + 'Path: ' + s + str2)
     btn.pack()
     Label(self.top, text=' ').pack()
 
-    _start_asr = Button(self.top, text=str2+'Start DTW'+str2)
+    _start_asr = Button(self.top, text=str2 + 'Start DTW' + str2)
     _start_asr.pack()
     _start_asr.config(font=("Courier", 12))
     Label(self.top, text=' ').pack(padx=190, pady=10)
@@ -117,35 +131,38 @@ def FunForButtonDTW(self):
     temp_Label = Label(frame, text='\n\n')
     temp_Label.pack(padx=250, pady=5)
 
-    def startGoogleASR(self):
+    def startDTW(self):
         temp_Label.destroy()
-        Label(frame, text='File: '+s+' text= My name is zvika.').pack()
+
+        emph_word = algo(file_path)
+
+        Label(frame, text='File: ' + s + ' text= '+emph_word).pack()
 
         Label(frame, text=' ').pack(padx=250, pady=5)
-    _start_asr.bind('<Button>', startGoogleASR)
+
+    _start_asr.bind('<Button>', startDTW)
 
 
 root = Tk()
 root.title("Final Project")
 
-Label(root, text='\nPerpetrators by: Aviad Atlas, Zvika Binyamin, Akiva Gabay and Asaf Cohen.                                                    \n').pack()
+Label(root,
+      text='\nPerpetrators by: Aviad Atlas, Zvika Binyamin, Akiva Gabay and Asaf Cohen.                                                    \n').pack()
 Label4 = Label(root, text='\n\n Voice Recognition & Voice Emphasis\nUsing “Deep Learning"')
 Label4.pack()
 Label4.config(font=("Courier", 16))
 
 Label(root, text=' ').pack(padx=10, pady=30, side=LEFT)
 
-
-button_ASR = Button(root, text=str1+'ASR'+str1)
+button_ASR = Button(root, text=str1 + 'ASR' + str1)
 button_ASR.pack(padx=20, pady=50, side=LEFT)
 button_ASR.bind('<Button>', FunForButtonASR)
 
-
-button_Classifier = Button(root, text=str1+'Classifier'+str1)
+button_Classifier = Button(root, text=str1 + 'Classifier' + str1)
 button_Classifier.pack(padx=20, pady=50, side=LEFT)
 button_Classifier.bind('<Button>', FunForButtonClassifier)
 
-button_DTW = Button(root, text=str1+'DTW'+str1)
+button_DTW = Button(root, text=str1 + 'DTW' + str1)
 button_DTW.pack(padx=20, pady=50, side=LEFT)
 button_DTW.bind('<Button>', FunForButtonDTW)
 
